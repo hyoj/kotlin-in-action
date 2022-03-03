4abc217b0a54298a30d9e01e227d970cde95022b
hyoj <chohyojoy@gmail.com>
2022-03-03 23:14:57 +0900

    chore: when 식의 인자는 아무 객체나 사용할 수 있음

--- src/ch02/2.3.3_UsingWhenWithArbitraryObjects.kt
+++ src/ch02/2.3.3_UsingWhenWithArbitraryObjects.kt
@@ -4,7 +4,7 @@ import ch02.colors.Color
 import ch02.colors.Color.*
 
 fun mix(c1: Color, c2: Color) =
-        when (setOf(c1, c2)) {
+        when (setOf(c1, c2)) { // when 식의 인자로 아무 객체를 사용할 수 있다.
             setOf(RED, YELLOW) -> ORANGE
             setOf(YELLOW, BLUE) -> GREEN
             setOf(BLUE, VIOLET) -> INDIGO
