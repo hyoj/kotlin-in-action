8c1439208c6391acb34a6552316a1d25418079d4
hyoj <chohyojoy@gmail.com>
2022-03-03 23:32:19 +0900

    chore: when 식에서 분기 조건을 검사할 때는 equality 비교를 사용함
    코틀린에서 ==는 값을 비교한다. 즉, 동등성(equality) 비교를 한다.
    코틀린에서 ===는 주소값을 비교한다. 즉, 동일성(identify) 비교를 한다.
    
    자바에서 ==는 원시 타입일 때는 값을 비교하고, 참조 타입일 때는 주소값을 비교한다.
    자바에서 참조 타입의 두 객체가 값이 같은지(동등성)를 알기 위해서 equals 메서드를 사용한다.
    
    주의! equals 메서드를 오버라이드 할 때는 이것과 호환되는 hashCode 메서드도 반드시 작성해야 한다.

--- src/ch02/2.3.3_UsingWhenWithArbitraryObjects.kt
+++ src/ch02/2.3.3_UsingWhenWithArbitraryObjects.kt
@@ -5,7 +5,7 @@ import ch02.colors.Color.*
 
 fun mix(c1: Color, c2: Color) =
         when (setOf(c1, c2)) { // when 식의 인자로 아무 객체를 사용할 수 있다.
-            setOf(RED, YELLOW) -> ORANGE
+            setOf(RED, YELLOW) -> ORANGE // 분기 조건에 있는 객체 사이를 매치할 때는 동등성(equality)을 사용한다.
             setOf(YELLOW, BLUE) -> GREEN
             setOf(BLUE, VIOLET) -> INDIGO
             else -> throw Exception("Dirty color")
